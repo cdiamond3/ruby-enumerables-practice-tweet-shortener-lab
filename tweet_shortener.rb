cf5f6226cf9c14825e582d2ca8ab04e9c58126f1
Chris Diamond <diamond.chris.3@gmail.com>
2021-06-09 17:51:18 -0600

    getting somewhere

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  dictionary = {
+    "too" => "2",
+    "to" => "2",
+    "two" => "2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split.map.collect do |word|
+    if dictionary.keys.include?(word)
+      word = dictionary[word]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+
